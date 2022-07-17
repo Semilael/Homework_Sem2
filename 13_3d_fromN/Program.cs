3998618571f417f0841593f53143b93e2279d215
Semilael <ariana-ses97@yandex.ru>
2022-07-17 22:52:40 +0300

    Project to find 3d digit in some number

---
+++ 13_3d_fromN/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Write a number with several digits in it: ");
+int aNumber = int.Parse(Console.ReadLine());
+string strNumber = Convert.ToString(aNumber);
+
+if (strNumber.Length > 2)
+{
+    Console.WriteLine("3d digit is: " + strNumber[2]);
+}
+else
+{
+    Console.WriteLine("Not a 3-digit number");
+}
