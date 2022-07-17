d687af89783254df5268e941f7ff562c89e021cc
Semilael <ariana-ses97@yandex.ru>
2022-07-17 23:05:41 +0300

    Project to say if weekend or weekday

---
+++ 15_Weekend/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Insert number of day of the week to check if it is a weekend: ");
+int weekDayNum = int.Parse(Console.ReadLine());
+
+if((weekDayNum == 6) ^(weekDayNum == 7))
+{
+    Console.WriteLine("It is a weekend, yahoo!");
+}
+else if(1 > weekDayNum || weekDayNum > 7)
+{
+    Console.WriteLine("It's not the day of the week.");
+}
+else
+{
+    Console.WriteLine("Oh, no! It's a weekday. You've gotta work!");
+}
