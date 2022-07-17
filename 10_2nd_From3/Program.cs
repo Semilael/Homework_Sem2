f73afe3af374a988ae15d86602262ee18d43f087
Semilael <ariana-ses97@yandex.ru>
2022-07-17 22:39:01 +0300

    Project to separate 2nd digit from 3-digital number

---
+++ 10_2nd_From3/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Write three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+int num2 = (number / 10) % 10;
+Console.WriteLine($"Now I show you 2nd digit: {num2}");
